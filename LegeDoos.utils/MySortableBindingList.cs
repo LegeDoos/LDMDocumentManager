6e0133e6acb18df12e3d5c6d3e8ab3b4592129e9
LegeDoos <rob@cilissen.nl>
2013-12-07 22:12:38 +0100

    Move MySortableBindingList to Utils

--- LegeDoos.utils/MySortableBindingList.cs
+++ LegeDoos.utils/MySortableBindingList.cs
@@ -6,7 +6,7 @@ using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LegeDoos.LDM
+namespace LegeDoos.Utils
 {
     /*
      * http://www.codeproject.com/Articles/31418/Implementing-a-Sortable-BindingList-Very-Very-Quic
